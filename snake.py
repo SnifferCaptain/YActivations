c734e116ba39e92b06d09de3fdbd3e7d7aae6dc0
SnifferCaptain <151126294+SnifferCaptain@users.noreply.github.com>
2025-03-27 16:53:45 +0800

    Add DytSnakeB

--- snake.py
+++ snake.py
@@ -21,3 +21,16 @@ class SnakeC(nn.Module):
 
     def forward(self, x):
         return torch.erf(x) + nn.functional.gelu(x, 'tanh')
+
+class DytSnakeB(nn.Module):
+    """ DytSnakeB: y=tanh(αx) + silu(x) """
+    def __init__(self, shape:list[int]=[1]):
+        super(DytSnakeB, self).__init__()
+        self.alpha = nn.Parameter(torch.randn(shape)*0.1+1, requires_grad=True)
+        self.register_full_backward_hook(self.hook)
+
+    def forward(self, x):
+        return torch.tanh(self.alpha * x) + nn.functional.silu(x)
+
+    def hook(self, module, grad_input, grad_output):
+        self.alpha.grad = self.alpha.grad * 0.1
